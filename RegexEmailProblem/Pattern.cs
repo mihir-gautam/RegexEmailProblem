ed662bd55b00ebed9d218c6ea92ebc1a3b6a8895
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-07 17:21:24 +0530

    [Mihir] Add Regex validation for country code in the end

--- RegexEmailProblem/Pattern.cs
+++ RegexEmailProblem/Pattern.cs
@@ -7,7 +7,7 @@ namespace RegexEmailProblem
 {
     class Pattern
     {
-        string Regex_Email = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-zA-Z]+[.][a-zA-Z]+$";
+        string Regex_Email = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2,})?$";
         public void validateEmail(string email)
         {
             if (Regex.IsMatch(email, Regex_Email))
